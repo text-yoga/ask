50878702f34a85c6f9a6696c223aafb6537cdb12
sigma-andex <77549848+sigma-andex@users.noreply.github.com>
2023-12-18 09:23:36 +0000

    Add text generation output

--- examples/remix/app/routes/_index.tsx
+++ examples/remix/app/routes/_index.tsx
@@ -2,24 +2,27 @@ import { useFetcher } from "@remix-run/react";
 import { useEffect, useState } from "react";
 import LlamaWorker from "../../node_modules/@text-yoga/ask/dist/index?worker";
 
-function updateStatus(data: any) {
-  switch (data.status) {
-    case "loading":
-      console.log("Loading");
-      break;
-    case "generating":
-      const { message, prompt, sentence, tokensSec, totalTime } = data;
-      console.log(`Generating ${message}`);
-      break;
-    case "complete":
-      console.log(`Done`);
-      break;
-  }
-}
+const updateStatus =
+  (data: any) => (dispatch: React.Dispatch<React.SetStateAction<string>>) => {
+    switch (data.status) {
+      case "loading":
+        console.log("Loading");
+        break;
+      case "generating":
+        const { message, prompt, sentence, tokensSec, totalTime } = data;
+        console.log(`Generating ${sentence}`);
+        dispatch(sentence);
+        break;
+      case "complete":
+        console.log(`Done`);
+        break;
+    }
+  };
 
 export default function Index() {
   const fetcher = useFetcher();
   const [state, setState] = useState<Worker | null>(null);
+  const [text, setText] = useState<string>("hello world");
   useEffect(() => {
     console.log(`Creating worker`);
     const llamaWorker = new LlamaWorker();
@@ -27,7 +30,7 @@ export default function Index() {
     const handleMessage = (event: any) => {
       const { status, error, message, prompt, sentence } = event.data;
       console.log(`Received message from worker`);
-      if (status) updateStatus(event.data);
+      if (status) updateStatus(event.data)(setText);
       if (error) {
         llamaWorker.removeEventListener("message", handleMessage);
         // reject(new Error(error));
@@ -41,11 +44,12 @@ export default function Index() {
         // resolve(event.data);
       }
     };
+    llamaWorker.addEventListener("message", handleMessage);
     llamaWorker.postMessage({
       weightsURL: `http://localhost:5173/model.bin`,
       modelID: "stories15M",
       tokenizerURL: `http://localhost:5173/tokenizer.json`,
-      prompt: "Hello world",
+      prompt: "What are basic steps in brewing beer?",
       temp: 0.4,
       top_p: 1,
       repeatPenalty: 1.1,
@@ -58,6 +62,7 @@ export default function Index() {
   return (
     <div>
       <h1>Test</h1>
+      <div>{text}</div>
       {/* <fetcher.Form method="post">
         <input
           name="question"
